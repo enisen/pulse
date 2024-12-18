252bd024c685b332bb2e4d3c49cd13d4001fe87d
Enis Şen <enis.sen@tomdigital.com.tr>
2024-12-18 17:15:47 +0300

    update

--- app/projects/[code]/page.tsx
+++ app/projects/[code]/page.tsx
@@ -1,10 +1,11 @@
+// app/projects/[code]/page.tsx
 import ProjectPlanner from '@/components/ProjectPlanningApp'
 
-export default function ProjectDetailPage({
-  params,
-}: {
-  params: { code: string }
-}) {
-  const { code } = params
+interface PageProps {
+  params: Promise<{ code: string }>
+}
+
+export default async function ProjectDetailPage({ params }: PageProps) {
+  const { code } = await params
   return <ProjectPlanner projectCode={code} />
 }
